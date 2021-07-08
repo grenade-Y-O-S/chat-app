501314015cbae6fde96376cb4bbc70322d8c7530
grenade-Y-O-S <crmson54@gmail.com>
2021-07-08 17:47:43 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-#  validates :email, presence: true
-#  validates :password, presence: true
-#  validates :password_confirmation, presence: true
+  #  validates :email, presence: true
+  #  validates :password, presence: true
+  #  validates :password_confirmation, presence: true
+  has_many :room_users
+  has_many :rooms, through: :room_users
 end
